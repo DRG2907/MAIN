4b02466c5a9c67fd0ae7af6a0b86843b85d2ec4b
DRG2907 <asdwdsaxs@gmail.com>
2026-08-05 20:02:34 +0300

    Add files via upload

---
+++ MENU.js
@@ -0,0 +1,7 @@
+  let num =Math.floor(Math.random() * (4));
+  if(num==1){var DIRECTION = prompt("WHERE ARE YOU HEADING?");
+
+  }
+  if(DIRECTION == "NOWHERE") {
+    document.write('<a href="https://drg2907.github.io/NOWHERE/"><div class="void"><div class="mid"><BAG><div class="shake" style="animation-delay: 0.1s"> THEN I WILL BE WAITING FOR YOU. </div></BAG></div></div></a>');
+  }
